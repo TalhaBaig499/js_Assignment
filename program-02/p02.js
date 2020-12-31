e21e599317a3f2b121c4ee34b9753003274c5931
Talha Latif Baig <75484836+TalhaBaig499@users.noreply.github.com>
2020-12-31 11:34:58 +0000

    Inchs into Centimeaters
    chnge height in inches into height in centemeaters

---
+++ program-02/p02.js
@@ -0,0 +1,25 @@
+// write a javascript program that gets person height from user in inches and convert it into 
+// centimeters using theg formula heighInCentimeters = 2.54 * height, console the output. 
+
+let height = 108;
+console.log ("Hinght in inche: " + height);
+const heighInCentimeters = 2.54 * height;
+console.log ("Height of a person in Centimeters= "+ heighInCentimeters);
+
+
+
+console.log ("----------Funtion OUtPUT--------------");
+
+
+
+
+// Using Function
+
+function person(){
+let height = 104;
+console.log ("Hinght in inche: " + height);
+const heighInCentimeters = 2.54 * height;
+console.log ("Height of a person in Centimeters= "+ heighInCentimeters);
+    
+}
+person();
