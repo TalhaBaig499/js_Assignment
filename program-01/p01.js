48cfa0867ed7082049fcad6675f0c906f15bbeb2
Talha Latif Baig <75484836+TalhaBaig499@users.noreply.github.com>
2020-12-31 11:08:44 +0000

    Celsius chnage in Fehrenheit
    I dont know how to get input from user i seracherd but i found that input through  html .

---
+++ program-01/p01.js
@@ -0,0 +1,8 @@
+
+
+// 1.  write a javascript program that gets temperature from user in 
+// celsius and convert it intog Fahrenheit using the formula F = 9/5 * C + 32, alert the output. 
+const c = 40;
+console.log("The given temperature in celsius is: " + c + " C");
+var f = 9/5 * c + 32;
+console.log("Your Temperature in Fahrenhiet is :" +f + " F");
