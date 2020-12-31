b6a03a2050605270d35941ff710f123f70f47a62
Talha Latif Baig <75484836+TalhaBaig499@users.noreply.github.com>
2020-12-31 12:29:45 +0000

    Marks and display msg
    display message if condition is true

---
+++ progrram-03/p03.js
@@ -0,0 +1,28 @@
+// write a program that inputs marks of three subjects.if the average of marks is more than 80,
+// itg displays message "you are above standard, Admission Granted!". 
+
+let Marks01 = 84;
+console.log ("This is English Compouter Phyicse Maeks= " + Marks01 );
+
+let Marks02 = 80;
+console.log ("This is English Compouter Phyicse Maeks= " +  Marks02 );
+
+let Marks03 = 70;
+console.log ("This is English Compouter Phyicse Maeks= " +  Marks03);
+if( Marks01>=80 && Marks02>=80) {
+    console.log("you are above standard, Admission Granted! ")
+}
+else
+{
+    console.log("Sorry! Your not achived standerd...");
+    console.log("Best of luck for next time!");
+}
+
+
+
+
+
+// sorry i dont know how to get input from user I searched but in found in throug html file
+
+// if you write marks03  you will see the "else block" message
+// if you remove marks03 OR (marks03+ marks02) you will see the "Admission granted" message
