d4bbed11de1f515a0a0d2ac24daf63aaf53a0f96
Talha Latif Baig <75484836+TalhaBaig499@users.noreply.github.com>
2021-01-01 16:19:22 +0000

    prime numbers

---
+++ program-04/p04.js
@@ -0,0 +1,22 @@
+// write a javascript program that inputs 
+// a number from the user and check whether it is prime number or not.
+
+let n = 9;
+let result;
+
+// for (let i=2; i <= n-1; i++) {
+//     if(n%i == 0) {
+//         result = false;
+//     }
+//     console.log(i);
+// }
+
+if (n%n == 0) {
+    console.log (n + " is not a prime number");
+} else {
+    console.log (n + "is a prime number");
+}
+
+
+
+// without input becouse input tag/file/var is working only with html file
